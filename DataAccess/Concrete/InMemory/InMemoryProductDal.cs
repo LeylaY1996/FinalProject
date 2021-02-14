e994c75a63bbc7c936a0b8d5d676869976fa60f5
Leyla <leylay1996@gmail.com>
2021-02-14 21:28:49 +0300

    added crud

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -2,30 +2,81 @@
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
 {
     public class InMemoryProductDal : IProductDal
     {
+
+        //veri varmış gibi davranıyoruz bu yüzden list oluşturuyoruz
+        List<Product> _products;
+
+        //uygulama çalıştığı anda constructor metodu çalışır
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{
+                    ProductId=1,
+                    CategoryId=1,
+                    ProductName="Bardak",
+                    UnitPrice=15,
+                    UnitsInStock=45
+                },
+                 new Product{
+                    ProductId=2,
+                    CategoryId=1,
+                    ProductName="Telefon",
+                    UnitPrice=150,
+                    UnitsInStock=25
+                },
+                  new Product{
+                    ProductId=3,
+                    CategoryId=2,
+                    ProductName="Klavye",
+                    UnitPrice=25,
+                    UnitsInStock=15
+                },
+                   new Product{
+                    ProductId=4,
+                    CategoryId=2,
+                    ProductName="Fare",
+                    UnitPrice=15,
+                    UnitsInStock=23
+                }
+            };
+        }
         public void Add(Product product)
         {
-            throw new NotImplementedException();
+            _products.Add(product);
         }
+
 
         public void Delete(Product product)
         {
-            throw new NotImplementedException();
+            //LINQ - Language Integrated Query
+            //Lambda
+            Product productToDelete =
+                 _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete); 
+
         }
 
         public List<Product> GetAll()
         {
-            throw new NotImplementedException();
+            return _products;
         }
 
         public void Update(Product product)
         {
-            throw new NotImplementedException();
+            //Gönderdiğim ürün id dini bul
+            Product productToUpdate =
+               _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
         }
     }
 }
